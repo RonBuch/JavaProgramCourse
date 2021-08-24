a890cda447165291b89e0f07b9f6a6ee5d3ec71b
Mike <73053764+RonBuch@users.noreply.github.com>
2021-08-24 11:47:34 +0700

    Добавлены и описаны объекты Point Rectangle Square

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,22 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+  public double ax;
+  public double ay;
+  public double bx;
+  public double by;
+  public double powerOf2;
+  public double AC;
+  public double BC;
+
+  public Point (double ax, double ay, double bx, double by, double powerOf2) {
+    this.ax = ax;
+    this.ay = ay;
+    this.bx = bx;
+    this.by = by;
+    this.powerOf2 = powerOf2;
+    this.AC = bx - ax;
+    this.BC = by - ay;
+  }
+
+}
