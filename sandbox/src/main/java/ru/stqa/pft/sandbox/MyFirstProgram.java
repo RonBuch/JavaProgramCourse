f273a87b2007f87e754e24fbfb3d44ddc848b394
Mike <73053764+RonBuch@users.noreply.github.com>
2021-08-24 11:48:13 +0700

    Написан метод для расчета расстояния м\у двумя точками на плоскости

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sandbox;
 
+import java.nio.channels.Pipe;
+
 public class MyFirstProgram {
 
   public static void main(String[] args) {
@@ -7,26 +9,31 @@ public class MyFirstProgram {
     hello("user");
     hello("Mike");
 
-    double l = 10;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-    double a = 10;
-    double b = 20;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    Rectangle rectangle = new Rectangle(10, 20);
+    System.out.println("Площадь прямоугольника со сторонами " + rectangle.a + " и " + rectangle.b + " = " + area(rectangle));
+
+    Point point = new Point(-2.1, 4.4, -10, 30, 2);
+    System.out.println("Расстояние между точкой А и В на плоскости = " + pointDistance(point) + " единиц измерения.");
   }
 
   public static void hello(String sombody) {
     System.out.println("Hello " + sombody + "!");
   }
 
-  public static double area(double len) {
-    return len * len;
+  public static double area(Square s) {
+    return s.l * s.l;
 
   }
 
-  public static double area(double a, double b) {
-    return a * b;
+  public static double area(Rectangle rectangle) {
+    return rectangle.a * rectangle.b;
   }
 
+  public static double pointDistance(Point point) {
+    return Math.sqrt((Math.pow(point.AC, point.powerOf2)) + (Math.pow(point.BC, point.powerOf2)));
+  }
 
 }
