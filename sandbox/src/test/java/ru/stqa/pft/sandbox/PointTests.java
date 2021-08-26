02c7ae31a59f90d6b4573b5b8aa2d73e32df9f10
Mike <73053764+RonBuch@users.noreply.github.com>
2021-08-26 12:27:30 +0700

    Изменены тесты в классе PointTests

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -5,6 +5,21 @@ import org.testng.annotations.Test;
 
 public class PointTests {
 
+  @Test
+  public void smoke(){
+    Point p1 = new Point(5,5);
+    Point p2 = new Point(-4,3);
+    Assert.assertEquals(p1.distance(p2), 46.87216658103186);
+  }
+
+  @Test
+  public void positive (){
+    Point p1 = new Point(50,50);
+    Point p2 = new Point(50,50);
+    Assert.assertEquals(p1.distance(p2), 0.0);
+  }
+
+
 //  @Test
 //  public void testDistance() {
 //
