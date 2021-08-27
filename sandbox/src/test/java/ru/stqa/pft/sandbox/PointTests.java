0804649b3cdc7985e6e8e7acd003f926cd59b9e8
Mike <73053764+RonBuch@users.noreply.github.com>
2021-08-27 18:48:47 +0700

    Загружен автотест из Katalon в пакет Addressbook

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -9,7 +9,7 @@ public class PointTests {
   public void smoke(){
     Point p1 = new Point(5,5);
     Point p2 = new Point(-4,3);
-    Assert.assertEquals(p1.distance(p2), 46.87216658103186);
+    Assert.assertEquals(p1.distance(p2), 9.219544457292887);
   }
 
   @Test
