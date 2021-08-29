49f847bbccf1fa302dbe191b7ece3b3f7f06a7e9
Mike <73053764+RonBuch@users.noreply.github.com>
2021-08-29 11:46:50 +0700

    Удалили ненужные переменыые и методы, переименовали driver в dw, поправели аннотации к Before and After

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,91 +1,69 @@
 
 package ru.stqa.pft.addressbook;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
+
 import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class GroupCreationTests {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+    private WebDriver dw;
 
-  @BeforeClass(alwaysRun = true)
-  public void setUp() throws Exception {
-    driver = new ChromeDriver();
-    baseUrl = "https://www.google.com/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
 
-  @Test
-  public void testGroupCreationTests() throws Exception {
-    driver.get("https://localhost/addressbook/group.php");
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.xpath("//input[@value='Login']")).click();
-    driver.findElement(By.linkText("groups")).click();
-    driver.findElement(By.name("new")).click();
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys("test1");
-    driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys("test2");
-    driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys("test3");
-    driver.findElement(By.name("submit")).click();
-    driver.findElement(By.linkText("group page")).click();
-    driver.findElement(By.linkText("Logout")).click();
-  }
-
-  @AfterClass(alwaysRun = true)
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() throws Exception {
+        dw = new ChromeDriver();
+        dw.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
-  }
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
+    @Test
+    public void testGroupCreation() throws Exception {
+        dw.get("https://localhost/addressbook/group.php");
+        dw.findElement(By.name("user")).click();
+        dw.findElement(By.name("user")).clear();
+        dw.findElement(By.name("user")).sendKeys("admin");
+        dw.findElement(By.name("pass")).clear();
+        dw.findElement(By.name("pass")).sendKeys("secret");
+        dw.findElement(By.xpath("//input[@value='Login']")).click();
+        dw.findElement(By.linkText("groups")).click();
+        dw.findElement(By.name("new")).click();
+        dw.findElement(By.name("group_name")).click();
+        dw.findElement(By.name("group_name")).clear();
+        dw.findElement(By.name("group_name")).sendKeys("test1");
+        dw.findElement(By.name("group_header")).clear();
+        dw.findElement(By.name("group_header")).sendKeys("test2");
+        dw.findElement(By.name("group_footer")).clear();
+        dw.findElement(By.name("group_footer")).sendKeys("test3");
+        dw.findElement(By.name("submit")).click();
+        dw.findElement(By.linkText("group page")).click();
+        dw.findElement(By.linkText("Logout")).click();
     }
-  }
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() throws Exception {
+        dw.quit();
     }
-  }
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
+
+    private boolean isElementPresent(By by) {
+        try {
+            dw.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
-  }
+
+    private boolean isAlertPresent() {
+        try {
+            dw.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+
 }
