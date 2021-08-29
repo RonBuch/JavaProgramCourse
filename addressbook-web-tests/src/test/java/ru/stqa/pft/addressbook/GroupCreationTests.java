67d8639f04554ec481cf08716c2622f04f8e3e59
Mike <73053764+RonBuch@users.noreply.github.com>
2021-08-29 12:15:18 +0700

    Проведен рефакторинг GroupCreationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -9,47 +9,71 @@ import org.testng.annotations.*;
 import org.openqa.selenium.*;
 
 public class GroupCreationTests {
-    private WebDriver dw;
+    private WebDriver wd;
 
 
     @BeforeMethod(alwaysRun = true)
     public void setUp() throws Exception {
-        dw = new ChromeDriver();
-        dw.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        dw.get("https://localhost/addressbook/group.php");
-        dw.findElement(By.name("user")).click();
-        dw.findElement(By.name("user")).clear();
-        dw.findElement(By.name("user")).sendKeys("admin");
-        dw.findElement(By.name("pass")).clear();
-        dw.findElement(By.name("pass")).sendKeys("secret");
-        dw.findElement(By.xpath("//input[@value='Login']")).click();
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd.get("https://localhost/addressbook/group.php");
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
 
     @Test
     public void testGroupCreation() throws Exception {
-        dw.findElement(By.linkText("groups")).click();
-        dw.findElement(By.name("new")).click();
-        dw.findElement(By.name("group_name")).click();
-        dw.findElement(By.name("group_name")).clear();
-        dw.findElement(By.name("group_name")).sendKeys("test1");
-        dw.findElement(By.name("group_header")).clear();
-        dw.findElement(By.name("group_header")).sendKeys("test2");
-        dw.findElement(By.name("group_footer")).clear();
-        dw.findElement(By.name("group_footer")).sendKeys("test3");
-        dw.findElement(By.name("submit")).click();
-        dw.findElement(By.linkText("group page")).click();
-        dw.findElement(By.linkText("Logout")).click();
+        gotoGroupPage();
+        initGroupCreation();
+        fillGroupForm(new GroupData("test1", "test2", "test3"));
+        submitGroupCreation();
+        returnToGroupPage();
+    }
+
+    private void returnToGroupPage() {
+        wd.findElement(By.linkText("group page")).click();
+        wd.findElement(By.linkText("Logout")).click();
+    }
+
+    private void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    private void fillGroupForm(GroupData groupData) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    private void initGroupCreation() {
+        wd.findElement(By.name("new")).click();
+    }
+
+    private void gotoGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
     }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() throws Exception {
-        dw.quit();
+        wd.quit();
     }
 
 
     private boolean isElementPresent(By by) {
         try {
-            dw.findElement(by);
+            wd.findElement(by);
             return true;
         } catch (NoSuchElementException e) {
             return false;
@@ -58,7 +82,7 @@ public class GroupCreationTests {
 
     private boolean isAlertPresent() {
         try {
-            dw.switchTo().alert();
+            wd.switchTo().alert();
             return true;
         } catch (NoAlertPresentException e) {
             return false;
