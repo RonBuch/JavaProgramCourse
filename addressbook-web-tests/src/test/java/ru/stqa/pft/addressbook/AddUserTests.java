0ad0c85f1fe4f3838e8c88211895c569d0ad3c35
Mike <73053764+RonBuch@users.noreply.github.com>
2021-08-30 23:00:54 +0700

    Рефакторинг. Назначены переменные в методы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddUserTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddUserTests.java
@@ -16,14 +16,14 @@ public class AddUserTests {
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
-    login();
+    login("admin", "secret");
   }
 
-  private void login() {
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.id("LoginForm")).submit();
   }
 
@@ -31,26 +31,26 @@ public class AddUserTests {
   public void testAddUser() {
 
     goToAddNewUserPage();
-    inputFirstName();
-    inputMiddleName();
-    inputLastName();
-    inputNickName();
-    inputTitle();
-    inputCompanyName();
-    inputAddress();
-    inputHome();
-    inputMobilePhoneNumber();
-    inputWorkPhoneNumber();
-    inputFaxNumber();
-    inputEmail1();
-    inputEmail2();
-    inputEmail3();
-    inputUserHomepage();
-    inputBirthDate();
-    inputAnniverseryDate();
-    inputSecondAddress();
-    inputSecondHomePhoneNumber();
-    inputSecondNotes();
+    inputFirstName("Kilo");
+    inputMiddleName("Gramm");
+    inputLastName("Mini");
+    inputNickName("1tonn");
+    inputTitle("Title");
+    inputCompanyName("Cjvpany");
+    inputAddress("Address 1");
+    inputHome("Sweet");
+    inputMobilePhoneNumber("Yes");
+    inputWorkPhoneNumber("Yes");
+    inputFaxNumber("+1705674673");
+    inputEmail1("Email@mail.com");
+    inputEmail2("mail@mail.ty");
+    inputEmail3("Hotmail@hotmail.com");
+    inputUserHomepage("www.leningradspb.ru");
+    inputBirthDate("9", "January", "1990");
+    inputAnniverseryDate("17", "October", "2020");
+    inputSecondAddress("Address 2");
+    inputSecondHomePhoneNumber("Home2");
+    inputSecondNotes("Some notes here");
     clicOnEnterButton();
     goToHomePage();
     clickSelectAllCheckBox();
@@ -84,116 +84,116 @@ public class AddUserTests {
     wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
   }
 
-  private void inputSecondNotes() {
+  private void inputSecondNotes(String noteText) {
     wd.findElement(By.name("notes")).clear();
-    wd.findElement(By.name("notes")).sendKeys("Some notes here");
+    wd.findElement(By.name("notes")).sendKeys(noteText);
   }
 
-  private void inputSecondHomePhoneNumber() {
+  private void inputSecondHomePhoneNumber(String secondPhoneNumber) {
     wd.findElement(By.name("phone2")).clear();
-    wd.findElement(By.name("phone2")).sendKeys("Home2");
+    wd.findElement(By.name("phone2")).sendKeys(secondPhoneNumber);
   }
 
-  private void inputSecondAddress() {
+  private void inputSecondAddress(String address2) {
     wd.findElement(By.name("address2")).click();
     wd.findElement(By.name("address2")).clear();
-    wd.findElement(By.name("address2")).sendKeys("Address 2");
+    wd.findElement(By.name("address2")).sendKeys(address2);
   }
 
-  private void inputAnniverseryDate() {
+  private void inputAnniverseryDate(String day, String month, String yaer) {
     wd.findElement(By.name("aday")).click();
-    new Select(wd.findElement(By.name("aday"))).selectByVisibleText("17");
+    new Select(wd.findElement(By.name("aday"))).selectByVisibleText(day);
     wd.findElement(By.name("amonth")).click();
-    new Select(wd.findElement(By.name("amonth"))).selectByVisibleText("October");
+    new Select(wd.findElement(By.name("amonth"))).selectByVisibleText(month);
     wd.findElement(By.name("ayear")).click();
     wd.findElement(By.name("ayear")).clear();
-    wd.findElement(By.name("ayear")).sendKeys("2020");
+    wd.findElement(By.name("ayear")).sendKeys(yaer);
   }
 
-  private void inputBirthDate() {
+  private void inputBirthDate(String day, String month, String yaer) {
     wd.findElement(By.name("bday")).click();
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText("9");
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(day);
     wd.findElement(By.name("bmonth")).click();
-    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText("January");
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(month);
     wd.findElement(By.name("byear")).click();
     wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys("1990");
+    wd.findElement(By.name("byear")).sendKeys(yaer);
   }
 
-  private void inputUserHomepage() {
+  private void inputUserHomepage(String homePageAdress) {
     wd.findElement(By.name("homepage")).clear();
-    wd.findElement(By.name("homepage")).sendKeys("www.leningradspb.ru");
+    wd.findElement(By.name("homepage")).sendKeys(homePageAdress);
   }
 
-  private void inputEmail3() {
+  private void inputEmail3(String email3) {
     wd.findElement(By.name("email3")).clear();
-    wd.findElement(By.name("email3")).sendKeys("Hotmail@hotmail.com");
+    wd.findElement(By.name("email3")).sendKeys(email3);
   }
 
-  private void inputEmail2() {
+  private void inputEmail2(String email2) {
     wd.findElement(By.name("email2")).clear();
-    wd.findElement(By.name("email2")).sendKeys("mail@mail.ty");
+    wd.findElement(By.name("email2")).sendKeys(email2);
   }
 
-  private void inputEmail1() {
+  private void inputEmail1(String email1) {
     wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("Email@mail.com");
+    wd.findElement(By.name("email")).sendKeys(email1);
   }
 
-  private void inputFaxNumber() {
+  private void inputFaxNumber(String faxNumber) {
     wd.findElement(By.name("fax")).clear();
-    wd.findElement(By.name("fax")).sendKeys("+1705674673");
+    wd.findElement(By.name("fax")).sendKeys(faxNumber);
   }
 
-  private void inputWorkPhoneNumber() {
+  private void inputWorkPhoneNumber(String workPhoneNumber) {
     wd.findElement(By.name("work")).clear();
-    wd.findElement(By.name("work")).sendKeys("Yes");
+    wd.findElement(By.name("work")).sendKeys(workPhoneNumber);
   }
 
-  private void inputMobilePhoneNumber() {
+  private void inputMobilePhoneNumber(String mobilePhoneNumber) {
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("Yes");
+    wd.findElement(By.name("mobile")).sendKeys(mobilePhoneNumber);
   }
 
-  private void inputHome() {
+  private void inputHome(String homePhoneNumber) {
     wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys("Sweet");
+    wd.findElement(By.name("home")).sendKeys(homePhoneNumber);
   }
 
-  private void inputAddress() {
+  private void inputAddress(String address) {
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("Address 1");
+    wd.findElement(By.name("address")).sendKeys(address);
   }
 
-  private void inputCompanyName() {
+  private void inputCompanyName(String companyName) {
     wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys("Cjvpany");
+    wd.findElement(By.name("company")).sendKeys(companyName);
   }
 
-  private void inputTitle() {
+  private void inputTitle(String titleText) {
     wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys("Title");
+    wd.findElement(By.name("title")).sendKeys(titleText);
   }
 
-  private void inputNickName() {
+  private void inputNickName(String NickName) {
     wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys("1tonn");
+    wd.findElement(By.name("nickname")).sendKeys(NickName);
   }
 
-  private void inputLastName() {
+  private void inputLastName(String lastname) {
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("Mini");
+    wd.findElement(By.name("lastname")).sendKeys(lastname);
   }
 
-  private void inputMiddleName() {
+  private void inputMiddleName(String middleName) {
     wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys("Gramm");
+    wd.findElement(By.name("middlename")).sendKeys(middleName);
   }
 
-  private void inputFirstName() {
+  private void inputFirstName(String firstName) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Kilo");
+    wd.findElement(By.name("firstname")).sendKeys(firstName);
   }
 
   private void goToAddNewUserPage() {
