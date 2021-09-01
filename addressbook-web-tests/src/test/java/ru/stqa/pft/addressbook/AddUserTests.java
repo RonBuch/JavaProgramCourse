a8b3d3799f0041e1e1e33bca763190981130eaff
Mike <73053764+RonBuch@users.noreply.github.com>
2021-09-01 09:22:18 +0700

    Провел рефакторинг класса AddUser

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddUserTests.java
@@ -0,0 +1,162 @@
+package ru.stqa.pft.addressbook;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.Select;
+
+public class AddUserTests {
+    private WebDriver wd;
+
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() throws Exception {
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/");
+        login("admin", "secret");
+    }
+
+    private void login(String user, String password) {
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(user);
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+    @Test
+    public void testAddUserTests() throws Exception {
+
+        initAddUser();
+        fillUserForm("First name", "Middle name", "Last name", "Nick name", "Title", "Company name", "Address 1", "homePhone", "mobilePhone", "workPhone", "faxPhone", "email1", "email2", "email3", "homepage", "10", "January", "1900", "11", "February", "1901", "Address 2", "home 2", "notes");
+        clickEnter();
+
+    }
+
+    private void clickEnter() {
+        wd.findElement(By.id("content")).click();
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    private void fillUserForm(String firstName, String middleName, String lastName, String nickName, String title, String companyName, String firstAddress, String homePhoneNumber, String mobilePhoneNumber, String workPhoneNumber, String faxPhoneNumber, String emailAddress1, String emailAddress2, String emailAddress3, String homepage, String birthDay, String birthMonth, String birthYear, String anniversaryDay, String anniversaryMonth, String anniversaryYear, String secondAddress, String secondPhoneNumber, String notes) {
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(firstName);
+        wd.findElement(By.name("middlename")).clear();
+        wd.findElement(By.name("middlename")).sendKeys(middleName);
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(lastName);
+        wd.findElement(By.name("nickname")).click();
+        wd.findElement(By.name("nickname")).clear();
+        wd.findElement(By.name("nickname")).sendKeys(nickName);
+        wd.findElement(By.name("title")).click();
+        wd.findElement(By.name("title")).clear();
+        wd.findElement(By.name("title")).sendKeys(title);
+        wd.findElement(By.name("company")).click();
+        wd.findElement(By.name("company")).clear();
+        wd.findElement(By.name("company")).sendKeys(companyName);
+        wd.findElement(By.name("address")).click();
+        wd.findElement(By.name("address")).clear();
+        wd.findElement(By.name("address")).sendKeys(firstAddress);
+        wd.findElement(By.name("home")).click();
+        wd.findElement(By.name("home")).clear();
+        wd.findElement(By.name("home")).sendKeys(homePhoneNumber);
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys(mobilePhoneNumber);
+        wd.findElement(By.name("work")).click();
+        wd.findElement(By.name("work")).clear();
+        wd.findElement(By.name("work")).sendKeys(workPhoneNumber);
+        wd.findElement(By.name("fax")).click();
+        wd.findElement(By.name("fax")).clear();
+        wd.findElement(By.name("fax")).sendKeys(faxPhoneNumber);
+        wd.findElement(By.name("email")).click();
+        wd.findElement(By.name("email")).clear();
+        wd.findElement(By.name("email")).sendKeys(emailAddress1);
+        wd.findElement(By.name("email2")).click();
+        wd.findElement(By.name("email2")).clear();
+        wd.findElement(By.name("email2")).sendKeys(emailAddress2);
+        wd.findElement(By.name("email3")).click();
+        wd.findElement(By.name("email3")).clear();
+        wd.findElement(By.name("email3")).sendKeys(emailAddress3);
+        wd.findElement(By.name("homepage")).click();
+        wd.findElement(By.name("homepage")).clear();
+        wd.findElement(By.name("homepage")).sendKeys(homepage);
+        wd.findElement(By.name("theform")).click();
+        wd.findElement(By.name("bday")).click();
+        new Select(wd.findElement(By.name("bday"))).selectByVisibleText(birthDay);
+        wd.findElement(By.name("bmonth")).click();
+        new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(birthMonth);
+        wd.findElement(By.name("byear")).click();
+        wd.findElement(By.name("byear")).clear();
+        wd.findElement(By.name("byear")).sendKeys(birthYear);
+        wd.findElement(By.name("aday")).click();
+        new Select(wd.findElement(By.name("aday"))).selectByVisibleText(anniversaryDay);
+        wd.findElement(By.name("amonth")).click();
+        new Select(wd.findElement(By.name("amonth"))).selectByVisibleText(anniversaryMonth);
+        wd.findElement(By.name("ayear")).click();
+        wd.findElement(By.name("ayear")).clear();
+        wd.findElement(By.name("ayear")).sendKeys(anniversaryYear);
+        wd.findElement(By.name("theform")).click();
+        wd.findElement(By.name("address2")).click();
+        wd.findElement(By.name("address2")).clear();
+        wd.findElement(By.name("address2")).sendKeys(secondAddress);
+        wd.findElement(By.name("phone2")).click();
+        wd.findElement(By.name("phone2")).clear();
+        wd.findElement(By.name("phone2")).sendKeys(secondPhoneNumber);
+        wd.findElement(By.name("notes")).click();
+        wd.findElement(By.name("notes")).clear();
+        wd.findElement(By.name("notes")).sendKeys(notes);
+    }
+
+    private void initAddUser() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() throws Exception {
+        wd.findElement(By.linkText("home page")).click();
+        wd.findElement(By.id("MassCB")).click();
+        wd.findElement(By.xpath("//input[@value='Delete']")).click();
+        wd.switchTo().alert().accept();
+        wd.findElement(By.linkText("Logout")).click();
+        wd.quit();
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+//    private String closeAlertAndGetItsText() {
+//        try {
+//            Alert alert = driver.switchTo().alert();
+//            String alertText = alert.getText();
+//            if (acceptNextAlert) {
+//                alert.accept();
+//            } else {
+//                alert.dismiss();
+//            }
+//            return alertText;
+//        } finally {
+//            acceptNextAlert = true;
+//        }
+//    }
+}
