f7d244ade5c242fd19c4896305d18e6cf4403f5d
Mike <73053764+RonBuch@users.noreply.github.com>
2021-09-01 09:29:00 +0700

    Завершен рефакторинг согласно заданию №4

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddUserData.java
@@ -0,0 +1,151 @@
+package ru.stqa.pft.addressbook;
+
+public class AddUserData {
+    private final String firstName;
+    private final String middleName;
+    private final String lastName;
+    private final String nickName;
+    private final String title;
+    private final String companyName;
+    private final String firstAddress;
+    private final String homePhoneNumber;
+    private final String mobilePhoneNumber;
+    private final String workPhoneNumber;
+    private final String faxPhoneNumber;
+    private final String emailAddress1;
+    private final String emailAddress2;
+    private final String emailAddress3;
+    private final String homepage;
+    private final String birthDay;
+    private final String birthMonth;
+    private final String birthYear;
+    private final String anniversaryDay;
+    private final String anniversaryMonth;
+    private final String anniversaryYear;
+    private final String secondAddress;
+    private final String secondPhoneNumber;
+    private final String notes;
+
+    public AddUserData(String firstName, String middleName, String lastName, String nickName, String title, String companyName, String firstAddress, String homePhoneNumber, String mobilePhoneNumber, String workPhoneNumber, String faxPhoneNumber, String emailAddress1, String emailAddress2, String emailAddress3, String homepage, String birthDay, String birthMonth, String birthYear, String anniversaryDay, String anniversaryMonth, String anniversaryYear, String secondAddress, String secondPhoneNumber, String notes) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+        this.title = title;
+        this.companyName = companyName;
+        this.firstAddress = firstAddress;
+        this.homePhoneNumber = homePhoneNumber;
+        this.mobilePhoneNumber = mobilePhoneNumber;
+        this.workPhoneNumber = workPhoneNumber;
+        this.faxPhoneNumber = faxPhoneNumber;
+        this.emailAddress1 = emailAddress1;
+        this.emailAddress2 = emailAddress2;
+        this.emailAddress3 = emailAddress3;
+        this.homepage = homepage;
+        this.birthDay = birthDay;
+        this.birthMonth = birthMonth;
+        this.birthYear = birthYear;
+        this.anniversaryDay = anniversaryDay;
+        this.anniversaryMonth = anniversaryMonth;
+        this.anniversaryYear = anniversaryYear;
+        this.secondAddress = secondAddress;
+        this.secondPhoneNumber = secondPhoneNumber;
+        this.notes = notes;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getFirstAddress() {
+        return firstAddress;
+    }
+
+    public String getHomePhoneNumber() {
+        return homePhoneNumber;
+    }
+
+    public String getMobilePhoneNumber() {
+        return mobilePhoneNumber;
+    }
+
+    public String getWorkPhoneNumber() {
+        return workPhoneNumber;
+    }
+
+    public String getFaxPhoneNumber() {
+        return faxPhoneNumber;
+    }
+
+    public String getEmailAddress1() {
+        return emailAddress1;
+    }
+
+    public String getEmailAddress2() {
+        return emailAddress2;
+    }
+
+    public String getEmailAddress3() {
+        return emailAddress3;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public String getBirthMonth() {
+        return birthMonth;
+    }
+
+    public String getBirthYear() {
+        return birthYear;
+    }
+
+    public String getAnniversaryDay() {
+        return anniversaryDay;
+    }
+
+    public String getAnniversaryMonth() {
+        return anniversaryMonth;
+    }
+
+    public String getAnniversaryYear() {
+        return anniversaryYear;
+    }
+
+    public String getSecondAddress() {
+        return secondAddress;
+    }
+
+    public String getSecondPhoneNumber() {
+        return secondPhoneNumber;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+}
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddUserTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddUserTests.java
@@ -32,7 +32,7 @@ public class AddUserTests {
     public void testAddUserTests() throws Exception {
 
         initAddUser();
-        fillUserForm("First name", "Middle name", "Last name", "Nick name", "Title", "Company name", "Address 1", "homePhone", "mobilePhone", "workPhone", "faxPhone", "email1", "email2", "email3", "homepage", "10", "January", "1900", "11", "February", "1901", "Address 2", "home 2", "notes");
+        fillUserForm(new AddUserData("First name", "Middle name", "Last name", "Nick name", "Title", "Company name", "Address 1", "homePhone", "mobilePhone", "workPhone", "faxPhone", "email1", "email2", "email3", "homepage", "10", "January", "1900", "11", "February", "1901", "Address 2", "home 2", "notes"));
         clickEnter();
 
     }
@@ -42,75 +42,75 @@ public class AddUserTests {
         wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
     }
 
-    private void fillUserForm(String firstName, String middleName, String lastName, String nickName, String title, String companyName, String firstAddress, String homePhoneNumber, String mobilePhoneNumber, String workPhoneNumber, String faxPhoneNumber, String emailAddress1, String emailAddress2, String emailAddress3, String homepage, String birthDay, String birthMonth, String birthYear, String anniversaryDay, String anniversaryMonth, String anniversaryYear, String secondAddress, String secondPhoneNumber, String notes) {
+    private void fillUserForm(AddUserData addUserData) {
         wd.findElement(By.name("firstname")).clear();
-        wd.findElement(By.name("firstname")).sendKeys(firstName);
+        wd.findElement(By.name("firstname")).sendKeys(addUserData.getFirstName());
         wd.findElement(By.name("middlename")).clear();
-        wd.findElement(By.name("middlename")).sendKeys(middleName);
+        wd.findElement(By.name("middlename")).sendKeys(addUserData.getMiddleName());
         wd.findElement(By.name("lastname")).click();
         wd.findElement(By.name("lastname")).clear();
-        wd.findElement(By.name("lastname")).sendKeys(lastName);
+        wd.findElement(By.name("lastname")).sendKeys(addUserData.getLastName());
         wd.findElement(By.name("nickname")).click();
         wd.findElement(By.name("nickname")).clear();
-        wd.findElement(By.name("nickname")).sendKeys(nickName);
+        wd.findElement(By.name("nickname")).sendKeys(addUserData.getNickName());
         wd.findElement(By.name("title")).click();
         wd.findElement(By.name("title")).clear();
-        wd.findElement(By.name("title")).sendKeys(title);
+        wd.findElement(By.name("title")).sendKeys(addUserData.getTitle());
         wd.findElement(By.name("company")).click();
         wd.findElement(By.name("company")).clear();
-        wd.findElement(By.name("company")).sendKeys(companyName);
+        wd.findElement(By.name("company")).sendKeys(addUserData.getCompanyName());
         wd.findElement(By.name("address")).click();
         wd.findElement(By.name("address")).clear();
-        wd.findElement(By.name("address")).sendKeys(firstAddress);
+        wd.findElement(By.name("address")).sendKeys(addUserData.getFirstAddress());
         wd.findElement(By.name("home")).click();
         wd.findElement(By.name("home")).clear();
-        wd.findElement(By.name("home")).sendKeys(homePhoneNumber);
+        wd.findElement(By.name("home")).sendKeys(addUserData.getHomePhoneNumber());
         wd.findElement(By.name("mobile")).click();
         wd.findElement(By.name("mobile")).clear();
-        wd.findElement(By.name("mobile")).sendKeys(mobilePhoneNumber);
+        wd.findElement(By.name("mobile")).sendKeys(addUserData.getMobilePhoneNumber());
         wd.findElement(By.name("work")).click();
         wd.findElement(By.name("work")).clear();
-        wd.findElement(By.name("work")).sendKeys(workPhoneNumber);
+        wd.findElement(By.name("work")).sendKeys(addUserData.getWorkPhoneNumber());
         wd.findElement(By.name("fax")).click();
         wd.findElement(By.name("fax")).clear();
-        wd.findElement(By.name("fax")).sendKeys(faxPhoneNumber);
+        wd.findElement(By.name("fax")).sendKeys(addUserData.getFaxPhoneNumber());
         wd.findElement(By.name("email")).click();
         wd.findElement(By.name("email")).clear();
-        wd.findElement(By.name("email")).sendKeys(emailAddress1);
+        wd.findElement(By.name("email")).sendKeys(addUserData.getEmailAddress1());
         wd.findElement(By.name("email2")).click();
         wd.findElement(By.name("email2")).clear();
-        wd.findElement(By.name("email2")).sendKeys(emailAddress2);
+        wd.findElement(By.name("email2")).sendKeys(addUserData.getEmailAddress2());
         wd.findElement(By.name("email3")).click();
         wd.findElement(By.name("email3")).clear();
-        wd.findElement(By.name("email3")).sendKeys(emailAddress3);
+        wd.findElement(By.name("email3")).sendKeys(addUserData.getEmailAddress3());
         wd.findElement(By.name("homepage")).click();
         wd.findElement(By.name("homepage")).clear();
-        wd.findElement(By.name("homepage")).sendKeys(homepage);
+        wd.findElement(By.name("homepage")).sendKeys(addUserData.getHomepage());
         wd.findElement(By.name("theform")).click();
         wd.findElement(By.name("bday")).click();
-        new Select(wd.findElement(By.name("bday"))).selectByVisibleText(birthDay);
+        new Select(wd.findElement(By.name("bday"))).selectByVisibleText(addUserData.getBirthDay());
         wd.findElement(By.name("bmonth")).click();
-        new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(birthMonth);
+        new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(addUserData.getBirthMonth());
         wd.findElement(By.name("byear")).click();
         wd.findElement(By.name("byear")).clear();
-        wd.findElement(By.name("byear")).sendKeys(birthYear);
+        wd.findElement(By.name("byear")).sendKeys(addUserData.getBirthYear());
         wd.findElement(By.name("aday")).click();
-        new Select(wd.findElement(By.name("aday"))).selectByVisibleText(anniversaryDay);
+        new Select(wd.findElement(By.name("aday"))).selectByVisibleText(addUserData.getAnniversaryDay());
         wd.findElement(By.name("amonth")).click();
-        new Select(wd.findElement(By.name("amonth"))).selectByVisibleText(anniversaryMonth);
+        new Select(wd.findElement(By.name("amonth"))).selectByVisibleText(addUserData.getAnniversaryMonth());
         wd.findElement(By.name("ayear")).click();
         wd.findElement(By.name("ayear")).clear();
-        wd.findElement(By.name("ayear")).sendKeys(anniversaryYear);
+        wd.findElement(By.name("ayear")).sendKeys(addUserData.getAnniversaryYear());
         wd.findElement(By.name("theform")).click();
         wd.findElement(By.name("address2")).click();
         wd.findElement(By.name("address2")).clear();
-        wd.findElement(By.name("address2")).sendKeys(secondAddress);
+        wd.findElement(By.name("address2")).sendKeys(addUserData.getSecondAddress());
         wd.findElement(By.name("phone2")).click();
         wd.findElement(By.name("phone2")).clear();
-        wd.findElement(By.name("phone2")).sendKeys(secondPhoneNumber);
+        wd.findElement(By.name("phone2")).sendKeys(addUserData.getSecondPhoneNumber());
         wd.findElement(By.name("notes")).click();
         wd.findElement(By.name("notes")).clear();
-        wd.findElement(By.name("notes")).sendKeys(notes);
+        wd.findElement(By.name("notes")).sendKeys(addUserData.getNotes());
     }
 
     private void initAddUser() {
