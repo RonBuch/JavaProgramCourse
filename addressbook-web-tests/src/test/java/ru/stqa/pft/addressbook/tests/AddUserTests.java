a44e2f8eb4abd39756d850a5bd26d6016dcaeec6
Mike <73053764+RonBuch@users.noreply.github.com>
2021-09-07 12:05:51 +0700

    Отформатирован код в AdddUserTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddUserTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddUserTests.java
@@ -13,7 +13,6 @@ public class AddUserTests extends TestBase {
         app.getAddUserHelper().fillUserForm(new AddUserData("First name", "Middle name", "Last name", "Nick name", "Title", "Company name", "Address 1", "homePhone", "mobilePhone", "workPhone", "faxPhone", "email1", "email2", "email3", "homepage", "10", "January", "1900", "11", "February", "1901", "Address 2", "home 2", "notes"));
         app.getAddUserHelper().clickEnter();
         app.getAddUserHelper().goToHomePageInAddUserPage();
-
         app.logout();
 
     }
