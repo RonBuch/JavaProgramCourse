a859ed2f65aa4ab2b4b561e016b87b74029e986f
Mike <73053764+RonBuch@users.noreply.github.com>
2021-09-16 09:29:55 +0700

    Добавлены проверки к тестам по удалению контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDelitionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDelitionTests.java
@@ -25,6 +25,9 @@ public class ContactDelitionTests extends TestBase {
         app.getNavigationHelper().goToHomePage();
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() - 1);
+
+        before.remove(before.size() - 1);
+        Assert.assertEquals(before, after);
     }
 
 
